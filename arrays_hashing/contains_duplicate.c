f2dc8f9025494996a1ad9f7e36996802a700c6f2
AomenX <amine.momenn@outlook.com>
2026-08-29 22:54:15 +0100

    solve contains duplicate using hash set

--- arrays_hashing/contains_duplicate.c
+++ arrays_hashing/contains_duplicate.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include "hash_set.h"
 
-int main(void)
+int contains_duplicate(int *nums, int size)
 {
-    t_HashSet set;
+        t_HashSet       set;
+        int     i;
 
-    if (!hash_set_init(&set, 10))
-        return (1);
+        hash_set_init(&set, size);
+        i = 0;
+        while (i < size)
+        {
+                if (hash_set_contains(&set, nums[i]))
+                {
+                        hash_set_destroy(&set);
+                        return (1);
+                }
+                hash_set_insert(&set, nums[i]);
+                i++;
+        }
+        hash_set_destroy(&set);
+        return (0);
+}
 
-    hash_set_insert(&set, 12);
-    hash_set_insert(&set, 42);
-    hash_set_insert(&set, 43);
-    hash_set_insert(&set, 25);
-    hash_set_insert(&set, 31);
+int     main(void)
+{
+        int nums1[] = {1, 2, 3, 1};
+        int nums2[] = {1, 2, 3, 4};
 
-    printf("42: %d\n", hash_set_contains(&set, 42));
-    printf("99: %d\n", hash_set_contains(&set, 99));
-    printf("43: %d\n", hash_set_contains(&set, 43));
-
-    return (0);
+        printf("%d\n", contains_duplicate(nums1, 4));
+        printf("%d\n", contains_duplicate(nums2, 4));
 }
